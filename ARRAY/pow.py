800bbb23269b433185c68578ab393c1ff35c0812
shelly2904 <iamshalinigangwar@gmail.com>
2016-09-19 21:40:03 +0530

    power

---
+++ ARRAY/pow.py
@@ -0,0 +1,20 @@
+def power(x, n):
+	if n==0:
+		return 1
+	return x*power(x, n-1)
+
+
+def power_rec(x, y):
+	temp = 0
+	if y == 0:
+		return 1
+	temp = power_rec(x, y/2);
+	if y%2 == 0:
+		return temp*temp;
+	else:
+		return x*temp*temp;
+
+
+if __name__=="__main__":
+	print power(3, 2)
+	print power_rec(3, 2)
