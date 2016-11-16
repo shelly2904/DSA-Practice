afe1e8e3792a144ac38366ef2fe7a52192a6f9b8
shelly2904 <iamshalinigangwar@gmail.com>
2016-11-16 21:09:42 +0530

    construct tree from inorder and preorder

--- TREE/construct_tree.py
+++ TREE/construct_tree.py
@@ -18,23 +18,20 @@ class Tree(object):
 		self.n = len(prearr)
 
 	def postorder(self, root):
+		if not root:
+			return
 		self.postorder(root.left)
-        self.postorder(root.right)
-        print root.data
+		self.postorder(root.right)
+		print root.data
 
 
 
 	def create_subarray(self, P, I):
-		#print "Printing the preorder: ", P
-		#print "Printing the inorder: ", I
 		if len(P) == 1:
-			#print "Element is ", P[0]
 			return Node(P[0])
 		node = P[0]
-		
 		n_idx = I.index(node)
 		root = Node(node)
-		#print "Node is : ", node
 		left = self.create_subarray(P[1:n_idx+1], I[0:n_idx])
 		right = self.create_subarray(P[n_idx+1:], I[n_idx+1:])
 		root.left, root.right = left, right 
@@ -42,13 +39,11 @@ class Tree(object):
 
 
 
-
 if __name__ == '__main__':
 	preorder = [1, 2, 4, 5, 3, 6, 7]
 	inorder = [4, 2, 5, 1, 6, 3, 7]
-
 	tree = Tree(inorder, preorder)
 	tree.root = tree.create_subarray(preorder,inorder)
-	print "post orfer tree: " 
+	print "post order tree: " 
 	tree.postorder(tree.root)
 	
