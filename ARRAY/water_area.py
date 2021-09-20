50487feb689d9f48a26838b314c5178e1a9d5044
Shalini Gangwar <iamshalinigangwar@gmail.com>
2021-09-20 13:25:22 +0530

    water area

---
+++ ARRAY/water_area.py
@@ -0,0 +1,20 @@
+"""
+LC: 11
+
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0).
+Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+"""
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        return None
+
+sol = Solution()
+print(sol.maxArea([1,8,6,2,5,4,8,3,7]))
+
+
