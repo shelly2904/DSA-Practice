f4f401e2e96d7f17638dd7c406514a4db09649f7
Shalini Gangwar <iamshalinigangwar@gmail.com>
2021-10-11 17:27:15 +0530

    patterns

--- PATTERNS/next_element_comparison.py
+++ PATTERNS/next_element_comparison.py
@@ -1,5 +1,5 @@
 """
-Foundation patters:
+Foundation patterns:
 1. Next Greater to the Right (NGR)
 2. Next Greater to the Left (NGL)
 3. Next Smaller to the Right (NSR)
@@ -9,14 +9,78 @@ Foundation patters:
 from collections import deque
 
 def ngr(arr):
-    pass
+
+    # Output arr
+    res = [-1] * len(arr)
+
+    # stack
+    stack = deque()
+
+    # to start from the end
+    for i in reversed(range(len(arr))):
+        while stack:
+            if stack[-1] <= arr[i]:
+                stack.pop()
+            else:
+                res[i] = stack[-1]
+                break
+        stack.append(arr[i])
+    return res
+
 
 def ngl(arr):
-    pass
+
+    # Output arr
+    res = [-1] * len(arr)
+
+    # stack
+    stack = deque()
+
+    # to start from the end
+    for i in range(len(arr)):
+        while stack:
+            if stack[-1] <= arr[i]:
+                stack.pop()
+            else:
+                res[i] = stack[-1]
+                break
+        stack.append(arr[i])
+    return res
 
 def nsr(arr):
-    pass
+    # Output arr
+    res = [-1] * len(arr)
 
+    # stack
+    stack = deque()
+
+    # to start from the end
+    for i in reversed(range(len(arr))):
+        while stack:
+            if stack[-1] > arr[i]:
+                stack.pop()
+            else:
+                res[i] = stack[-1]
+                break
+        stack.append(arr[i])
+    return res
 
 def nsl(arr):
-    pass
+    # Output arr
+    res = [-1] * len(arr)
+
+    # stack
+    stack = deque()
+
+    # to start from the end
+    for i in range(len(arr)):
+        while stack:
+            if stack[-1] > arr[i]:
+                stack.pop()
+            else:
+                res[i] = stack[-1]
+                break
+        stack.append(arr[i])
+    return res
+
+print(nsl([6,0,5,7,1,9]))
