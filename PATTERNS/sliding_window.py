9bf814b74f1ffb3b8e1975f9069c0d81b29031d3
Shalini Gangwar <iamshalinigangwar@gmail.com>
2021-10-13 23:10:05 +0530

    sliding window patterns

--- PATTERNS/sliding_window.py
+++ PATTERNS/sliding_window.py
@@ -50,14 +50,50 @@ def first_neg_k(arr, k):
                 queue.pop()
         else:
             ans.append(0)
-    
+
     return ans
 
 
 
+"""
+Count of occurences of Anagrams
+"""
+
+from collections import defaultdict
+from copy import deepcopy
+def count_anagrams(string, pat):
+
+    hash_map = defaultdict(int)
+    k = len(pat)  # window size
+    n = len(string)
+
+    cnt = 0
+
+    for i in pat:
+        hash_map[i] += 1
+
+    i = 0
+    j = 0
+
+    temp_hashmap = deepcopy(hash_map)
+    while j < n:
+        temp_hashmap[string[j]] -= 1
+        if j - i + 1 < k:
+            j += 1
+            continue
+        if j - i + 1 == k:
+            if list(set(temp_hashmap.values()))[0] == 0:
+                cnt += 1
+                temp_hashmap = deepcopy(hash_map)
+
+            i += 1
+            j = i
+
+    return cnt
+
+print(count_anagrams("aabaaabaa", "aaba"))
 
 
-
-print(first_neg_k([-8,2,3,-6,10], 2))
+#print(first_neg_k([-8,2,3,-6,10], 2))
 #print(max_sub_k([2,5,1,8,2,9,1], 3))
 
