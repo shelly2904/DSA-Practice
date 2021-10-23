092879348bf80278ae45fc776c2f143ba5759604
Shalini Gangwar <iamshalinigangwar@gmail.com>
2021-10-23 12:49:58 +0530

    binary search patterns

--- PATTERNS/binary_search_patterns_1.py
+++ PATTERNS/binary_search_patterns_1.py
@@ -13,11 +13,41 @@ Find floor and ceil of element in sorted array
 
 
 def find_floor(arr, key):
-    pass
+    low = 0
+    high = len(arr) - 1
+    n = len(arr)
+    floor = -1
+
+    while low <= high:
+
+        mid = low + (high - low) // 2
+        if arr[mid] == key:
+            return arr[mid]
+        elif arr[mid] > key:
+            high = mid - 1
+        else:
+            floor = arr[mid]
+            low = mid + 1
+    return floor
 
 
 def find_ceil(arr, key):
-    pass
+    low = 0
+    high = len(arr) - 1
+    n = len(arr)
+    ceil = -1
+
+    while low <= high:
+
+        mid = low + (high - low) // 2
+        if arr[mid] == key:
+            return arr[mid]
+        elif arr[mid] < key:
+            low = mid + 1
+        else:
+            ceil = arr[mid]
+            high = mid - 1
+    return ceil
 
 
 """
@@ -49,3 +79,8 @@ Minimum difference element
 
 def minimum_diff(arr, key):
     pass
+
+
+# Floor
+print("Floor is", find_floor([1,2,3,4,6,7,8], 5))
+print("Ceil is", find_ceil([1,2,3,4,6,7,8], 5))
