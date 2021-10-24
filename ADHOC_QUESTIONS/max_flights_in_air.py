3b3464b6db78208c3da144a8b8cf0e1948c775b6
Shalini Gangwar <iamshalinigangwar@gmail.com>
2021-10-24 16:23:45 +0530

    more ques

--- ADHOC_QUESTIONS/max_flights_in_air.py
+++ ADHOC_QUESTIONS/max_flights_in_air.py
@@ -9,21 +9,28 @@ Maximum number of flights in air
 from queue import PriorityQueue
 
 
+class Flight:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+
 def max_flights_air(arr):
     max_flights = 0
 
     pq = PriorityQueue()
 
     # Sorting by start time
-    arr = sorted(arr, key=lambda x: x[0])
+    arr = sorted(arr, key=lambda x: x.start)
+
     for i in arr:
-        while not pq.empty() and pq.queue[0][1] < i[0]:
+        while not pq.empty() and pq.queue[0][1] < i.start:
             pq.get()
-        pq.put(i)
+        pq.put((i.start, i.end))
         max_flights = max(max_flights, len(pq.queue))
 
     return max_flights
 
 
-input = [[4, 8], [2, 5], [17, 20], [10, 21], [9, 18]]
+input = [Flight(4, 8), Flight(2, 5), Flight(17, 20), Flight(10, 21), Flight(9, 18)]
 print(max_flights_air(input))
